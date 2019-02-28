768194a3eb9c74a2668cd61541b16f8ba4f7b05f
M. Shaaban <m.shaaban@mail.utoronto.ca>
2019-02-28 17:01:39 -0500

    updated solver to include a reporter

--- solver.cc
+++ solver.cc
@@ -29,3 +29,18 @@ void rhs(const state_type &x, state_type &dxdt, const double /*t*/)
     dxdt[1] = -C * x[1] * x[2] + E * x[0] * x[1];
     dxdt[2] = B * x[0] * x[2] + C * x[1] * x[2] - A * x[1] * x[2];
 }
+
+struct push_back_state_and_time
+{
+    std::vector<state_type> &m_states;
+    std::vector<double> &m_times;
+
+    push_back_state_and_time(std::vector<state_type> &states, std::vector<double> &times)
+        : m_states(states), m_times(times) {}
+
+    void operator()(const state_type &x, double t)
+    {
+        m_states.push_back(x);
+        m_times.push_back(t);
+    }
+};
