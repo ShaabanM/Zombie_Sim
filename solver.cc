e8f1ef40f109543958cfb6391bbbe4c67aa0b054
M Shaaban <m.shaaban@mail.utoronto.ca>
2019-03-03 11:32:12 -0500

    Implemented cmd output indicating who wins

--- solver.cc
+++ solver.cc
@@ -2,7 +2,6 @@
 #include "solver.h"
 #include "append_netcdf.h"
 
-
 // even if there is no explicit t dependence, one has to define t as a function parameter.
 void rhs(const state_type &x, state_type &dxdt, const double /*t*/)
 {
@@ -23,4 +22,14 @@ void solve_and_report(size_t steps, int dat_count, std::vector<state_type> x_vec
         data[i][3] = x_vec[i][2];
     }
     append_netcdf(data, dat_count);
+
+    if (x_vec[steps][0] + x_vec[steps][1] < 1)
+    {
+        cout <<  "Humanity Loses for Z0 = " << dat_count << endl;
+    }
+
+    if (x_vec[steps][2] < 1)
+    {
+        cout <<  "Humanity Wins for Z0 = " << dat_count << endl;
+    }
 }
